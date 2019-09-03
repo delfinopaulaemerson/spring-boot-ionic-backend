ae914f43da06b70078ec99f8f57457c36b1794b2
delfinopaulaemerson <delfs@hotmail.com>
2019-09-03 11:06:07 -0300

    criando operacao de instanciacao

--- src/main/java/com/curso/springboot/CursoMcApplication.java
+++ src/main/java/com/curso/springboot/CursoMcApplication.java
@@ -1,13 +1,37 @@
 package com.curso.springboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.curso.springboot.domain.Categoria;
+import com.curso.springboot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	/**
+	 * metodo implemantado pelo comandlineruner 
+	 * para inicializacao do springboot e execucao
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1  = new Categoria(null, "informatica");
+		Categoria cat2  = new Categoria(null, "escritorio");
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
