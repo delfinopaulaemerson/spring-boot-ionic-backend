cf10ccc3e0ed83b77b68b59d81a2703848aa77aa
delfinopaulaemerson <delfs@hotmail.com>
2019-09-03 12:07:17 -0300

    produto e associacao de muitos para muitos

--- src/main/java/com/curso/springboot/CursoMcApplication.java
+++ src/main/java/com/curso/springboot/CursoMcApplication.java
@@ -1,6 +1,8 @@
 package com.curso.springboot;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,13 +10,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.curso.springboot.domain.Categoria;
+import com.curso.springboot.domain.Produto;
 import com.curso.springboot.repositories.CategoriaRepository;
+import com.curso.springboot.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoMcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -27,11 +34,25 @@ public class CursoMcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
+		
 		Categoria cat1  = new Categoria(null, "informatica");
 		Categoria cat2  = new Categoria(null, "escritorio");
 		
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mause", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		this.categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		
+		this.produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
