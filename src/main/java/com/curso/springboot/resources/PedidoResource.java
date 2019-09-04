2062ac7c207bcf2e9d6adda5cae5a854d30ada26
delfinopaulaemerson <delfs@hotmail.com>
2019-09-04 08:33:49 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/curso/springboot/resources/PedidoResource.java
@@ -0,0 +1,31 @@
+package com.curso.springboot.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.springboot.domain.Categoria;
+import com.curso.springboot.domain.Pedido;
+import com.curso.springboot.services.CategoriaService;
+import com.curso.springboot.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws Exception {
+		Pedido obj = null;
+			
+		obj = this.service.find(id);
+		
+		return ResponseEntity.ok(obj);
+	}
+
+}
---
+++ src/main/java/com/curso/springboot/services/PedidoService.java
@@ -0,0 +1,31 @@
+package com.curso.springboot.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.springboot.domain.Categoria;
+import com.curso.springboot.domain.Pedido;
+import com.curso.springboot.repositories.CategoriaRepository;
+import com.curso.springboot.repositories.PedidoRepository;
+import com.curso.springboot.services.exception.ObjectNotFoundException;
+
+
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	
+	public Pedido find(Integer id) throws Exception {
+		
+		Optional<Pedido> obj = this.repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(" Objeto não encontrado! ID: " + id +", Tipo "+ Pedido.class.getName()));
+		
+	}
+
+}
