867eeee81d440a6e9dda1c02d266b095df05ec99
delfinopaulaemerson <delfs@hotmail.com>
2019-09-04 10:11:58 -0300

    inserindo uma nova categoria com post

--- src/main/java/com/curso/springboot/resources/CategoriaResource.java
+++ src/main/java/com/curso/springboot/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.curso.springboot.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.springboot.domain.Categoria;
 import com.curso.springboot.services.CategoriaService;
@@ -17,6 +21,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+	URI uri;
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) throws Exception {
 		Categoria obj = null;
@@ -24,6 +30,19 @@ public class CategoriaResource {
 		obj = this.service.find(id);
 		
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		
+		obj = service.insert(obj);
+		
+		//retorna a nova uri com o id do objeto criado
+		this.uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
+		 
 	}
 
 }
